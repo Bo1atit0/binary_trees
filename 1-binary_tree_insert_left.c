4ffb38531519b77c211a69398858caa32fb5d193
bo1atit0 <shorungbebolatito@gmail.com>
2024-01-31 15:41:29 +0100

    betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,6 +12,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
 
+    if (parent == NULL)
+    {
+        return (NULL);
+    }
+
 	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (newnode == NULL)
 	{
