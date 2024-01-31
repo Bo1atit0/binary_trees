accb634d68fffa7c3dfb4c89a978d6b610392d67
bo1atit0 <shorungbebolatito@gmail.com>
2024-01-31 06:30:07 -0800

    betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,28 +6,28 @@
  * @parent: pointer to the node to insert the left-child in
  * @value: the value to store in the new node
  * Return: newnode
-*/
+ */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newnode;
+	binary_tree_t *newnode;
 
-    newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-    if (newnode == NULL)
-    {
-        return (NULL);
-    }
-        newnode->parent = parent;
-        newnode->right = NULL;
-        newnode->left = NULL;
-        newnode->n = value;
+	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->parent = parent;
+	newnode->right = NULL;
+	newnode->left = NULL;
+	newnode->n = value;
 
-    if (parent->left != NULL)
-    {
-        newnode->left = parent->left;
-        newnode->parent = parent;
-    }
-parent->left = newnode;
-return (newnode);
+	if (parent->left != NULL)
+	{
+		newnode->left = parent->left;
+		newnode->parent = parent;
+	}
+	parent->left = newnode;
+	return (newnode);
 
 }
