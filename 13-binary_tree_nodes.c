500803a98fe8f9c4b86159379a4d76126d2aa39a
bo1atit0 <shorungbebolatito@gmail.com>
2024-02-02 15:01:39 -0800

    count nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,20 +9,21 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t nodes = 0;
-if (tree == NULL)
-{
-    return(0);
-}
+	size_t nodes = 0;
 
-if (tree->right != NULL || tree->left != NULL)
-{
-    nodes++;
-}
-nodes += binary_tree_nodes(tree->left);
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-nodes += binary_tree_nodes(tree->right);
+	if (tree->right != NULL || tree->left != NULL)
+	{
+		nodes++;
+	}
+	nodes += binary_tree_nodes(tree->left);
 
-return (nodes);
+	nodes += binary_tree_nodes(tree->right);
+
+	return (nodes);
 
 }
