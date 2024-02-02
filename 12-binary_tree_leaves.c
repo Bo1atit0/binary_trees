d040cd9d35672008705a80f91a6ee5e32324cdae
bo1atit0 <shorungbebolatito@gmail.com>
2024-02-02 04:09:57 -0800

    leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,20 +8,20 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t left, right;
+	size_t left, right;
 
-if (tree == NULL)
-{
-    return (0);
-}
-if (tree->left == NULL && tree->right == NULL)
-{
-    return (1);
-}
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
 
-left = binary_tree_leaves(tree->left);
-right = binary_tree_leaves(tree->right);
+	left = binary_tree_leaves(tree->left);
+	right = binary_tree_leaves(tree->right);
 
-return (left + right);
+	return (left + right);
 
 }
